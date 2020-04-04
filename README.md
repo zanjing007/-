5becd8db796ebbf4c264536095c6f945d0fb3ffb
zanjing007 <chenzanjing0822@163.com>
2020-04-04 22:47:14 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # -
 vue实现的一个简易音乐播放器
+
+功能：1、根据关键字搜索出音乐列表
+      2、点击播放，显示对应的歌曲图片，评论
+      3、点击歌曲MV即可播放，点击MV其他地方，关掉MV
+      4、天天音乐播放器，默认加载陈奕迅歌曲
+
+  1:歌曲搜索接口
+    请求地址:https://autumnfish.cn/search
+    请求方法:get
+    请求参数:keywords(查询关键字)
+    响应内容:歌曲搜索结果
+
+  2:歌曲url获取接口
+    请求地址:https://autumnfish.cn/song/url
+    请求方法:get
+    请求参数:id(歌曲id)
+    响应内容:歌曲url地址
+  3.歌曲详情获取
+    请求地址:https://autumnfish.cn/song/detail
+    请求方法:get
+    请求参数:ids(歌曲id)
+    响应内容:歌曲详情(包括封面信息)
+  4.热门评论获取
+    请求地址:https://autumnfish.cn/comment/hot?type=0
+    请求方法:get
+    请求参数:id(歌曲id,地址中的type固定为0)
+    响应内容:歌曲的热门评论
+  5.mv地址获取
+    请求地址:https://autumnfish.cn/mv/url
+    请求方法:get
+    请求参数:id(mvid,为0表示没有mv)
+    响应内容:mv的地址
+*/
--- js/main.js
+++ js/main.js
@@ -1,124 +1,104 @@
-/*
-  1:歌曲搜索接口
-    请求地址:https://autumnfish.cn/search
-    请求方法:get
-    请求参数:keywords(查询关键字)
-    响应内容:歌曲搜索结果
+// 设置axios的基地址
+axios.defaults.baseURL = 'https://autumnfish.cn';
+// axios.defaults.baseURL = 'http://localhost:3000';
 
-  2:歌曲url获取接口
-    请求地址:https://autumnfish.cn/song/url
-    请求方法:get
-    请求参数:id(歌曲id)
-    响应内容:歌曲url地址
-  3.歌曲详情获取
-    请求地址:https://autumnfish.cn/song/detail
-    请求方法:get
-    请求参数:ids(歌曲id)
-    响应内容:歌曲详情(包括封面信息)
-  4.热门评论获取
-    请求地址:https://autumnfish.cn/comment/hot?type=0
-    请求方法:get
-    请求参数:id(歌曲id,地址中的type固定为0)
-    响应内容:歌曲的热门评论
-  5.mv地址获取
-    请求地址:https://autumnfish.cn/mv/url
-    请求方法:get
-    请求参数:id(mvid,为0表示没有mv)
-    响应内容:mv的地址
-*/
+// 实例化vue
 var app = new Vue({
   el: "#player",
   data: {
-    // 查询关键字
-    query: "",
-    // 歌曲数组
+    // 搜索关键字,默认进来加载陈奕迅
+    query: '陈奕迅',
+    // 歌曲列表
     musicList: [],
-    // 歌曲地址
-    musicUrl: "",
-    // 歌曲封面
-    musicCover: "",
-    // 歌曲评论
+    // 歌曲url
+    musicUrl: '',
+    // 是否正在播放
+    isPlay: false,
+    // 歌曲热门评论
     hotComments: [],
-    // 动画播放状态
-    isPlaying: false,
-    // 遮罩层的显示状态
-    isShow: false,
+    // 歌曲封面地址
+    coverUrl: '',
+    // 显示视频播放
+    showVideo: false,
     // mv地址
-    mvUrl: ""
+    mvUrl: ''
   },
+  mounted () {
+    this.searchMusic()
+  },
+  // 方法
   methods: {
-    // 歌曲搜索
-    searchMusic: function() {
-      var that = this;
-      axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
-        function(response) {
-          // console.log(response);
-          that.musicList = response.data.result.songs;
-          console.log(response.data.result.songs);
-        },
-        function(err) {}
-      );
-    },
-    // 歌曲播放
-    playMusic: function(musicId) {
-      //   console.log(musicId);
-      var that = this;
-      // 获取歌曲地址
-      axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.data[0].url);
-          that.musicUrl = response.data.data[0].url;
-        },
-        function(err) {}
-      );
+    // 搜索歌曲
+    searchMusic() {
+      if (this.query == 0) {
+        return
+      }
+      axios.get('/search?keywords=' + this.query).then(response => {
+        // 保存内容
+        this.musicList = response.data.result.songs;
 
-      // 歌曲详情获取
-      axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.songs[0].al.picUrl);
-          that.musicCover = response.data.songs[0].al.picUrl;
-        },
-        function(err) {}
-      );
+    })
 
-      // 歌曲评论获取
-      axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.hotComments);
-          that.hotComments = response.data.hotComments;
-        },
-        function(err) {}
-      );
+      // 清空搜索
+      this.query = ''
     },
-    // 歌曲播放
-    play: function() {
-      // console.log("play");
-      this.isPlaying = true;
+    // 播放歌曲
+    playMusic(musicId) {
+      // 获取歌曲url
+      axios.get('/song/url?id=' + musicId).then(response => {
+        // 保存歌曲url地址
+        this.musicUrl = response.data.data[0].url
+    })
+      // 获取歌曲热门评论
+      axios.get('/comment/hot?type=0&id=' + musicId).then(response => {
+        // console.log(response)
+        // 保存热门评论
+        this.hotComments = response.data.hotComments
+
+    })
+      // 获取歌曲封面
+      axios.get('/song/detail?ids=' + musicId).then(response => {
+        // console.log(response)
+        // 设置封面
+        this.coverUrl = response.data.songs[0].al.picUrl
+    })
+
     },
-    // 歌曲暂停
-    pause: function() {
-      // console.log("pause");
-      this.isPlaying = false;
+    // audio的play事件
+    play() {
+      this.isPlay = true
+      // 清空mv的信息
+      this.mvUrl = ''
+    },
+    // audio的pause事件
+    pause() {
+      this.isPlay = false
     },
     // 播放mv
-    playMV: function(mvid) {
-      var that = this;
-      axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
-        function(response) {
-          // console.log(response);
-          console.log(response.data.data.url);
-          that.isShow = true;
-          that.mvUrl = response.data.data.url;
-        },
-        function(err) {}
-      );
+    playMv(vid) {
+      if (vid) {
+        this.showVideo = true;
+        // 获取mv信息
+        axios.get('/mv/url?id=' + vid).then(response => {
+          // console.log(response)
+          // 暂停歌曲播放
+          this.$refs.audio.pause()
+        // 获取mv地址
+        this.mvUrl = response.data.data.url
+      })
+      }
     },
-    // 隐藏
-    hide: function() {
-      this.isShow = false;
+    // 关闭mv界面
+    closeMv() {
+      this.showVideo = false
+      this.$refs.video.pause()
+    },
+    // 搜索历史记录中的歌曲
+    historySearch(history) {
+      this.query = history
+      this.searchMusic()
+      this.showHistory = false;
     }
-  }
-});
+  },
+
+})
